bd0128e37183912a35135aa708f0853952533461
Joshua Adu-Larbi Addy <138405695+Joshjha@users.noreply.github.com>
2025-05-22 02:49:29 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // ✅'react-dom/client' is used instead of just 'react-dom'
+import ReactDOM from 'react-dom/client'; //'react-dom/client' is used instead of just 'react-dom'
 
 import App from './App';
 
